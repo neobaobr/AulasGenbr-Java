2678a467249e39cdfbb0dd74182cd94c5dbc8613
Vitor Borela <borel4.lpv@gmail.com>
2020-11-12 19:43:19 -0300

    Primeira aula Java

---
+++ Main-hello/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String historia = "Hobbies de Vitao Borela : escutar musicas , produzir musicas , jogar tenis , ler textos , assistir series , pesquisar a respeito de novas tecnologias , tomar bons drinks , aprender sobre sustentabilidade , cultivo organico , tomar cafe , treinar , estudar , beber agua , ser alto , trocar lampadas , apreciar uns sorvetinhos e observar o por do sol e a lua :D " ;
+		String habilidade = "hailidade : orientaao ao futuro , mentalidade de crescimento , comunicacao , criativdade , proatividade ";
+				System.out.println(historia);
+				System.out.println(habilidade);
+	}
+
+}
